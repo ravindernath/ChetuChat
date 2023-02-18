61065f5d16282b2f3001ac70c777d2c8d7526275
Ravinder Nath <ravi89nath@gmail.com>
2023-02-19 00:53:22 +0530

    Integrate Login and SignUp feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import { ChakraProvider } from '@chakra-ui/react'
 import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/chat" component={ChatPage} />
-      </Container>
+      <ChakraProvider>
+        <Container>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/chat" component={ChatPage} />
+        </Container>
+      </ChakraProvider>
     </div>
   );
 }
