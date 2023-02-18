a479aec250b6afa4c3de2c25d635bf0abaff75e3
Ravinder Nath <ravi89nath@gmail.com>
2023-02-18 23:07:59 +0530

    Backend update for Error Handling

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
-// const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 app.use(express.json()); //To accept JSON Data
 
-app.get('/', (req, res) =>{
-    res.send("API is running !!!")
-})
+app.get("/", (req, res) => {
+  res.send("API is running !!!");
+});
 
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats)
-})
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/chat/:id', (req, res) => {
-    console.log(req.params.id)
-    const singleChat = chats.find( (c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+// app.get('/api/chat', (req, res) => {
+//     res.send(chats)
+// })
 
-const PORT = process.env.PORT || 4000
+// app.get('/api/chat/:id', (req, res) => {
+//     console.log(req.params.id)
+//     const singleChat = chats.find( (c) => c._id === req.params.id);
+//     res.send(singleChat);
+// })
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log(`server started on PORT ${PORT}`.yellow));
