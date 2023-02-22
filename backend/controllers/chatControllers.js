b6322e5adf73b75b213f73cc00e7cfc3f8bac3b2
Ravinder Nath <ravindern@chetu.com>
2023-02-22 23:27:37 +0530

    Fetch Chats api create

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -2,7 +2,6 @@ const asyncHandler = require("express-async-handler");
 const Chat = require("../models/chatModel");
 const User = require("../models/userModel");
 
-
 const accessChat = asyncHandler(async (req, res) => {
   const { userId } = req.body;
 
@@ -49,5 +48,24 @@ const accessChat = asyncHandler(async (req, res) => {
   }
 });
 
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user.id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
 
-module.exports = { accessChat };
+module.exports = { accessChat, fetchChats };
